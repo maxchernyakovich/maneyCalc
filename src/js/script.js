d12a31a64d4abe1394d8a0b3aff7f4c247d99980
Max <chernyakovich.96@mail>
2020-04-23 23:19:02 +0300

    last commit, add function event

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 
-
+const generateId = () => `maxProId${Math.round(Math.random() * 1e8).toString(16)}`;
 
 const totalBalance = document.querySelector('.total__balance'),
      totalMoneyIncome = document.querySelector('.total__money-income'),
@@ -7,47 +7,110 @@ const totalBalance = document.querySelector('.total__balance'),
      historyList = document.querySelector('.history__list'),
      operationName = document.querySelector('.operation__name'),
      operationAmount = document.querySelector('.operation__amount'),
-     form = document.getElementById('#form');
+     //form = document.getElementById('#form');
+     form = document.querySelector('.form__submit');
 
-let dbOperation = [
-     {
-          id: '1',
-          description: 'Получил зарплату',
-          amount: 30000,
-     },
-     {
-          id: '2',
-          description: 'Долг',
-          amount: -1000,
-     },
-     {
-          id: '3',
-          description: 'Купить продукты',
-          amount: -3000,
-     },
-     {
-          id: '4',
-          description: 'Купить одежду',
-          amount: -5000,
-     },
-     {
-          id: '5',
-          description: 'Заказ на фрилансе',
-          amount: 5000,
-     },
-];
+let dbOperation = JSON.parse(localStorage.getItem('calc')) || [];
 
-const renderOperation = () => {
+const renderOperation = (operation) => {
+
+     const className = operation.amount < 0 ?
+          'history__item-minus' :
+          'history__item-plus';
 
      const listItem = document.createElement('li');
      listItem.classList.add('history__item');
-     listItem.innerHTML = `
-     <span class="history__money">+30000 ₽</span>
-     <button class="history_delete">x</button>
+     listItem.classList.add(className);
+     listItem.innerHTML = `${operation.description}
+     <span class="history__money">${operation.amount}</span>
+     <button class="history_delete" data-id= "${operation.id}">x</button>
      `;
 
      historyList.append(listItem);
 
 };
 
-renderOperation();
+// деление элементов массива dbOperation на 2 разных массива и вывод их на страницу
+const updateBalance = () => {
+
+     const resultIncome = dbOperation
+     .filter((item) => item.amount > 0)
+     .reduce((result, item) => result + item.amount, 0);
+
+
+     const resultExpenses = dbOperation
+     .filter((item) => item.amount < 0)
+     .reduce((result, item) => result + item.amount, 0);
+
+     totalMoneyIncome.textContent = resultIncome + '₽';
+     totalMoneyExpenses.textContent = resultExpenses + '₽';
+     totalBalance.textContent = (resultIncome + resultExpenses) + '₽'; 
+
+}
+
+const addOperation = (event) => {
+     event.preventDefault();
+
+     const operationNameValue = operationName.value,
+          operationAmountValue = operationAmount.value;
+
+     operationName.style.borderColor = '';
+     operationAmount.style.borderColor = '';
+
+     if (operationNameValue !== '' && operationAmountValue !== '') {
+
+          const operation = {
+               id: generateId(),
+               description: operationNameValue,
+               amount: +operationAmountValue,
+          };
+
+          dbOperation.push(operation);
+          init();
+     } else {
+          if (!operationNameValue) operationName.style.borderColor = 'red';
+          if (!operationAmountValue) operationAmount.style.borderColor = 'red';
+     }
+
+     operationName.value = '';
+     operationAmount.value = '';
+};
+
+// удаляет элементы
+const deleteOperation = (event) => {
+
+     const target = event.target;
+
+     if (target.classList.contains('history_delete')) {
+          dbOperation = dbOperation
+               .filter((operation) => operation.id !== target.dataset.id);
+          init();
+     }
+
+
+};
+
+const init = () => {
+
+     historyList.textContent = '';
+
+     dbOperation.forEach(renderOperation);
+     // тоже самое что
+     /* dbOperation.forEach((item, i, arr) => {
+          renderOperation(item);
+     });*/
+     // и тоже самое что
+     /* for (let i = o; i < dbOperation.length; i++) {
+          renderOperation(dbOperation[i]);
+     }; */
+
+     updateBalance();
+
+     localStorage.setItem('calc', JSON.stringify(dbOperation));
+
+};
+
+form.addEventListener('submit', addOperation);
+historyList.addEventListener('click', deleteOperation);
+
+init();
